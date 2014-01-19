1124bfee743170e094aeba46441595bab9ba7513
kenrett <kenrettberg@gmail.com>
2014-01-19 10:01:21 -0800

    setup db and routes

--- app/models/earthquake.rb
+++ app/models/earthquake.rb
@@ -1,2 +1,16 @@
 class Earthquake < ActiveRecord::Base
+  attr_accessible :time,
+                  :latitude,
+                  :longitude,
+                  :depth,
+                  :mag,
+                  :magtype,
+                  :nst,
+                  :gap,
+                  :dmin,
+                  :rms,
+                  :net,
+                  :id,
+                  :updated,
+                  :place
 end
