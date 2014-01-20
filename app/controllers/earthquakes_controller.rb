df654fcb7dc4259a2b030ce15f541c6a9b510980
kenrett <kenrettberg@gmail.com>
2014-01-20 11:25:35 -0800

    messing with ActiveRecord::Base

--- app/controllers/earthquakes_controller.rb
+++ app/controllers/earthquakes_controller.rb
@@ -1,3 +1,11 @@
 class EarthquakesController < ApplicationController
+  include ActionController::MimeResponds
 
+  respond_to :json
+
+  def index
+    scope = Earthquake.all
+
+    scope = scope.day(Time.zone.at(params[:on].to_i).to_date) if params[:on]
+  end
 end
--- app/models/earthquake.rb
+++ app/models/earthquake.rb
@@ -1,16 +1,11 @@
 class Earthquake < ActiveRecord::Base
-  attr_accessible :time,
-                  :latitude,
-                  :longitude,
-                  :depth,
-                  :mag,
-                  :magtype,
-                  :nst,
-                  :gap,
-                  :dmin,
-                  :rms,
-                  :net,
-                  :id,
-                  :updated,
-                  :place
+  attr_accessible :time, :latitude, :longitude,:depth, :mag, :magtype, :nst, :gap, :dmin, :rms, :net, :id, :updated, :place
+
+  default_scope -> { where arel_table[:time].gteq(7.days.ago) }
+  scope :day, -> (time) { where arel_table[:time].gt(time.to_date) }
+
+  def self.filter_by_params(params)
+    # scoped = self.scoped
+    # scoped = scoped.where(:)
+  end
 end
